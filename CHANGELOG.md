9d9067d98de95e045b5e1c3425916e7d9dd57e7e
Sixto Martin <pitbulk@gmail.com>
2013-09-11 17:11:21 +0200

    Add Changelog, update version of the saml plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+
+CHANGELOG
+=========
+
+Auth plugin 11-07-2013
+----------------------
+
+ * Fix redirection issue
--- auth/saml/version.php
+++ auth/saml/version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2013180303;
+$plugin->version = 2013110701;
 $plugin->requires = 2010112400;;
-$plugin->release = 2013180303;
+$plugin->release = 2013110701;
 $plugin->maturity = 100;
