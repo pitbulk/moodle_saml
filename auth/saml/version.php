9711b6edbda1e2bb0003b69176b8cc79747de648
Jan Aagaard Meier <janzeh@gmail.com>
2014-11-24 09:21:34 +0100

    Release new version of auth

--- auth/saml/version.php
+++ auth/saml/version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2013110701;
-$plugin->requires = 2010112400;;
+$plugin->version = 2014112401;
+$plugin->requires = 2013040500;
 $plugin->release = 2013110701;
 $plugin->maturity = 100;
