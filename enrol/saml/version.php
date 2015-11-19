e0db0d09f1c654119834068e43a904e7b309c5cf
Jarosław Maciejewski <nitro.bystrzyca@gmail.com>
2015-11-19 17:13:39 +0100

    Update version.php

--- enrol/saml/version.php
+++ enrol/saml/version.php
@@ -4,4 +4,4 @@ $plugin->version = 2015072001;
 $plugin->requires = 2012062500;
 $plugin->release = 'enrol/saml version of 20-07-2015';
 $plugin->maturity = MATURITY_BETA;
-
+$plugin->component = 'enrol_saml';
